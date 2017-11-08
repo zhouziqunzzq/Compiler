a6f0144048a74999b971e5bc6248f7b1db7fe7c5
yuki <yukiyukiyu@github.com>
2017-11-08 16:35:28 +0800

    Add SymbolTable.

---
+++ Table/Category.h
@@ -0,0 +1,13 @@
+#ifndef CATEGORY_H
+#define CATEGORY_H
+
+enum Category
+{
+	CC = 1,
+	SC = 2,
+	IC = 3,
+	FC = 4,
+	V = 5
+};
+
+#endif
---
+++ Table/Table.h
@@ -0,0 +1,34 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include<vector>
+#include<map>
+using namespace std;
+
+template<class T>
+class Table
+{
+	private:
+		vector<T> table;
+		map<T, int> index;
+	
+	public:
+		int fill(T v);
+		bool get(int id, T &v);
+};
+
+template<class T>
+int Table<T>::fill(T v)
+{
+	auto it = index.find(v);
+	if(it != index.end())
+		return it->second;
+	else
+	{
+		table.push_back(v);
+		index[v]=table.size();
+		return table.size()-1;
+	}
+}
+
+#endif
